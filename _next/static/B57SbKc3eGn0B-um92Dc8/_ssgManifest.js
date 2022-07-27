b3ea4d19cba2d8269ee06ef02b1e43e2e626095c
Ricardo Stuven <rstuven@gmail.com>
2022-07-26 22:41:32 -0400

    Updates

---
+++ _next/static/B57SbKc3eGn0B-um92Dc8/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Facerca-de","\u002Fsocial\u002F[fragmentoId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/pHVYNtqV54DMF-uygVtTl/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Facerca-de","\u002Ffragmento\u002F[fragmentoId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()

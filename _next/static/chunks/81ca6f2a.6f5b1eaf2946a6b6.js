915bc72541758b81fe547bee5e532de0b8f59dee
Ricardo Stuven <rstuven@gmail.com>
2022-07-27 21:14:47 -0400

    Updates

---
+++ _next/static/chunks/81ca6f2a.6f5b1eaf2946a6b6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[935],{3022:function(t,n,e){e.d(n,{v8y:function(){return u}});var r=e(8281);function u(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",stroke:"#000",strokeWidth:"2",d:"M7,7 L17,17 M7,17 L17,7"}}]})(t)}}}]);
